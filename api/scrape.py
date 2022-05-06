7802fa8e3891ff9ac46175bc01c1b90bf43401b1
apostolistriantafyllou <67221448+apostolistriantafyllou@users.noreply.github.com>
2022-05-06 19:20:20 +0300

    Update scrape.py

--- api/scrape.py
+++ api/scrape.py
@@ -88,6 +88,47 @@ class rpiLoc:
             if status != 200:
                 raise Exception("API response: {}".format(status))
             return data
+    @staticmethod
+    def pi4_8gig_stockcheck():
+        URL = f"https://rpilocator.com/?cat=PI4"
+        status = get_status(URL)
+        response = get_soup(URL)
+
+        # find table get th and td into rows
+        tbody = response.find("tbody")
+        containers = tbody.find_all("tr")
+
+        if containers:
+            api = []
+            for container in containers:
+                cells = container.find_all("td")
+                product = cells[0].text
+                url = cells[1].find('a')['href']
+                update_status = cells[2].find("i")["class"][2]
+                vendor = cells[3].text
+                in_stock = cells[4].text
+                price = "$" + cells[6].text[6:]
+
+                if update_status == "text-success":
+                    if "8GB" in product:
+                        api.append(
+                            {
+                                "product": product,
+                                "url": url,
+                                "vendor": vendor,
+                                "in_stock": in_stock,
+                                "price": price,
+                            }
+
+                        )
+            data = {"status": status, "data": api}
+
+            if status != 200:
+                raise Exception("API response: {}".format(status))
+            if api.in_stock == "Yes":
+                return api.url
+            else:
+                return 'false'
 
     @staticmethod
     def get_rss_entires(region: str):
